f83fd42c9d8334c29125e3d7a2da6aed9b9a8ac3
Salman Alam <salman@fostrum.com>
2019-10-17 03:03:23 +0600

    Full Vuejs App

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>\{{ msg }}</h1>
+    <Search></Search>
+  </div>
+</template>
+
+<script>
+import Search from './Search'
+export default {
+  name: 'app',
+  components: {
+    Search
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style{{#sass}} lang="scss"{{/sass}}>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Search.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h3>New App for Capco Aziz</h3>
+    </div>
+</template>
+
+<script>
+</script>
